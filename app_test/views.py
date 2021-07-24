71d7e1a4c874327bdd3b715b477948f030fbdba7
yassan-h <yasuhiro.mpr@gmail.com>
2021-07-24 18:57:53 +0900

    Implement user account

--- app_test/views.py
+++ app_test/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
+from django.contrib.auth import login
+from django.http import HttpResponseRedirect
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+from .forms import SignUpForm
 
+class SignUp(CreateView):
+    form_class = SignUpForm
+    template_name = "registration/signup.html" 
+    success_url = reverse_lazy('app_test:index')
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        self.object = user 
+        return HttpResponseRedirect(self.get_success_url())
 
 def index(request):
     return render(request, 'app_test/index.html')
--- django_projects/settings.py
+++ django_projects/settings.py
@@ -127,3 +127,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
+
+LOGIN_URL='accounts/login/'
+LOGIN_REDIRECT_URL = 'app_test:index'
